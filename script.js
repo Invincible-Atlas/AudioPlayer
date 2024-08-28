5e130c014a267a16ff6cb7b9127a5d701e0bb787
Invincible-Atlas <148077776+Invincible-Atlas@users.noreply.github.com>
2024-08-28 18:04:43 +0000

    added seek bar

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+let timeChangeValue = 0;
+let seekAllowed = true;
+function secondsToTime(seconds){
+    let minutes = Math.floor(seconds / 60);
+    let seconds2 = Math.floor(seconds - minutes * 60);
+    if(seconds2.toString().length<2){
+        seconds2 = `0${seconds2}`;
+    }
+    return(`${minutes}:${seconds2}`);
+}
 function playToggle(){
     const player = document.getElementById("mainPlayer");
     const playButton = document.getElementById("playButton");
     const PBContent = document.getElementById("PBContent");
+    
     if(player.paused){
         player.play()
         playButton.style.backgroundColor = "green";
@@ -14,9 +25,41 @@ function playToggle(){
 }
 function audioBack(){
     const player = document.getElementById("mainPlayer");
-    player.currentTime = player.currentTime - 5;
+    timeChangeValue = timeChangeValue - 5;
 }
 function audioForward(){
     const player = document.getElementById("mainPlayer");
-    player.currentTime = player.currentTime + 5;
+    timeChangeValue = timeChangeValue + 5;
 }
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+function dynamicUpdates(){
+    const player = document.getElementById("mainPlayer");
+    const playButton = document.getElementById("playButton");
+    const PBContent = document.getElementById("PBContent");
+    const seekSlider = document.getElementById("seekSlider");
+    const tMinusText = document.getElementById("tMinusText");
+    const tPlusText = document.getElementById("tPlusText");
+    let newSliderValue=scale(player.currentTime,0,player.duration,1,1000);
+    if(Math.abs(newSliderValue-seekSlider.value)>2&&timeChangeValue==0&&seekAllowed==true){
+        player.currentTime=scale(seekSlider.value,1,1000,0,player.duration);
+    }else{
+        if(timeChangeValue!==0){
+            player.currentTime=player.currentTime+timeChangeValue;
+            timeChangeValue=0
+            newSliderValue=scale(player.currentTime,0,player.duration,1,1000);
+        }
+        
+        seekSlider.value=newSliderValue;
+    }
+    timeElapsed = secondsToTime(player.currentTime);
+    totalTime = secondsToTime(player.duration);
+    timeLeft = secondsToTime(player.duration-player.currentTime)
+    tPlusText.innerHTML=`${timeElapsed}/${totalTime}`;
+    
+}
+// document.onload=(ev)=>{
+setInterval(dynamicUpdates,100);
+
+// };
